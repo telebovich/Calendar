49dd969f8af1c7b4f9165245f40e8fe929a8bcb2
Robert Lebovich <robert.lebovich@gmail.com>
2019-06-28 01:42:29 +0300

    Improved design of create event screen

--- Calendar/wwwroot/js/create-event.js
+++ Calendar/wwwroot/js/create-event.js
@@ -1,1 +1,13 @@
-﻿$(".datepicker").datepicker({});
+﻿$(function () {
+    $(".datepicker").datepicker({});
+
+    $("#show_end_date_panel").change(function () {
+        if ($("#show_end_date_panel").is(":checked")) {
+            $("#event_end_panel").removeClass("d-none");
+            $("#event_end_panel").addClass("d-block");
+        } else {
+            $("#event_end_panel").removeClass("d-block");
+            $("#event_end_panel").addClass("d-none");
+        }
+    });
+});
