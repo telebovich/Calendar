42f67d0eabfe481b00f6db55ab357f637db1a708
Robert Lebovich <robert.lebovich@gmail.com>
2019-12-11 15:11:54 +0200

    Created a repository to work with IndexedDB

--- Calendar/wwwroot/js/create-event.js
+++ Calendar/wwwroot/js/create-event.js
@@ -1,29 +1,31 @@
 ﻿$(function () {
-    let db;
+    //let db;
 
-    let request = window.indexedDB.open('events_db', 2);
+    //let request = window.indexedDB.open('events_db', 2);
 
-    request.onerror = function () {
-        console.log('Database failed to open');
-    };
+    //request.onerror = function () {
+    //    console.log('Database failed to open');
+    //};
 
-    request.onsuccess = function () {
-        console.log('Database opened successfully');
+    //request.onsuccess = function () {
+    //    console.log('Database opened successfully');
 
-        db = request.result;
-    };
+    //    db = request.result;
+    //};
 
-    request.onupgradeneeded = function (e) {
-        db = e.target.result;
+    //request.onupgradeneeded = function (e) {
+    //    db = e.target.result;
 
-        let objectStore = db.createObjectStore('events_os', { keyPath: 'id', autoIncrement: true });
+    //    let objectStore = db.createObjectStore('events_os', { keyPath: 'id', autoIncrement: true });
 
-        // Define what data items the objectStore will contain
-        objectStore.createIndex('event_start', 'event_start', { unique: false });
-        objectStore.createIndex('event_end', 'event_end', { unique: false });
+    //    // Define what data items the objectStore will contain
+    //    objectStore.createIndex('event_start', 'event_start', { unique: false });
+    //    objectStore.createIndex('event_end', 'event_end', { unique: false });
 
-        console.log('Database setup complete');
-    };
+    //    console.log('Database setup complete');
+    //};
+
+    var eventsRepository = new EventsRepository();
 
     $(".datepicker").datepicker({});
 
@@ -43,20 +45,7 @@
         event_start.setMinutes($('#event_time_min').val());
 
         let newItem = { event_start };
-        let transaction = db.transaction(['events_os'], 'readwrite');
-        let objectStore = transaction.objectStore('events_os');
-        let request = objectStore.add(newItem);
-        request.onsuccess = function () {
-            console.log('Data added successfully');
-        };
-
-        request.onerror = function () {
-            console.log('Failure adding data');
-        };
-
-        request.oncomplete = function () {
-            console.log('Transaction completed: database modification finished.');
-        };
-
+        
+        eventsRepository.add(newItem);
     });
 });
---
+++ Calendar/wwwroot/js/events-repository.js
@@ -0,0 +1,48 @@
+﻿class EventsRepository {
+    constructor() {
+        this.db = null;
+        var fakethis = this;
+
+        let request = window.indexedDB.open('events_db', 2);
+
+        request.onerror = function () {
+            console.log('Database failed to open');
+        };
+
+        request.onsuccess = function () {
+            console.log('Database opened successfully');
+
+            fakethis.db = request.result;
+        };
+
+        request.onupgradeneeded = function (e) {
+            fakethis.db = e.target.result;
+
+            let objectStore = fakethis.db.createObjectStore('events_os', { keyPath: 'id', autoIncrement: true });
+
+            // Define what data items the objectStore will contain
+            objectStore.createIndex('event_start', 'event_start', { unique: false });
+            objectStore.createIndex('event_end', 'event_end', { unique: false });
+
+            console.log('Database setup complete');
+        };
+    }
+
+    add(event) {
+        let transaction = this.db.transaction(['events_os'], 'readwrite');
+        let objectStore = transaction.objectStore('events_os');
+        let request = objectStore.add(event);
+
+        request.onsuccess = function () {
+            console.log('Data added successfully');
+        };
+
+        request.onerror = function () {
+            console.log('Failure adding data');
+        };
+
+        request.oncomplete = function () {
+            console.log('Transaction completed: database modification finished.');
+        };
+    }
+}
